76e08157fd717d6cb990b7adad3460dc8c287a4b
Sukh Amar <B231940125@gmail.com>
2025-03-25 23:18:32 +0800

    web3

--- NomadLivestockDemo.java
+++ NomadLivestockDemo.java
@@ -4,8 +4,18 @@ public class NomadLivestockDemo {
         herd.addLivestock(new Horse("Bayn", 4));
         herd.addLivestock(new Sheep("Chuluun", 2));
         herd.addLivestock(new Camel("Tamir", 6));
-        herd.addLivestock(new Goat("yamaa", 4));
+        herd.addLivestock(new Goat("yamaa", 3));
 
         herd.dailyRoutine();
+        Livestock[] livestockArray = {
+            new Horse("Bayn", 4),
+            new Sheep("Chuluun", 2),
+            new Camel("Tamir", 6),
+            new Goat("yamaa", 3)
+        };
+
+        for (Livestock animal : livestockArray) {
+            animal.graze();
+        }
     }
 }
